ae5ad6107def33ce1d57cb0456bea6112731b7f6
jtrancoso-alt <117353028+jtrancoso-alt@users.noreply.github.com>
2024-07-04 14:20:37 +0200

    first working version
    - removed go files
    - changed to node.js and puppeteer
    - added readme instructions
    - updated gitignore

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # factorial-clock-in
+
+This project is a script to automate the Factorial clock in and out using Node.js and Puppeteer.
+It checks if the clocking is already started and clicks on the corresponding button.
+
+To run it you will need to:
+
+## 1. Install Dependencies:
+
+Intall Node.js and Puppeteer by running these commands:
+
+```ruby
+sudo apt update
+sudo apt install nodejs npm
+```
+
+```ruby
+npm install puppeteer dotenv
+```
+
+## 2. Set up environment varialbes
+
+Create a .env file in your project directory:
+
+```ruby
+touch .env
+```
+
+And add your email and password variables:
+
+```ruby
+EMAIL="your@email.com"
+PASSWORD="yourpassword"
+```
+
+## 3. Run the Script
+
+Run it with:
+
+```ruby
+node factorial_clock_in.js
+```
---
+++ factorial_clock_in.js
@@ -0,0 +1,57 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  console.log("Script Starts");
+  const page = await browser.newPage();
+
+  await page.goto("https://app.factorialhr.com/dashboard");
+
+  // Fill in the login form
+  var email = process.env.EMAIL;
+  var pass = process.env.PASSWORD;
+  await page.type("#user_email", email);
+  await page.type("#user_password", pass);
+
+  // Click the login button
+  await page.click('input[name="commit"]');
+
+  // Wait for navigation to the dashboard
+  await page.waitForNavigation();
+
+  console.log("Login Succesful");
+
+  // If not clocked in, start timing
+  // If already clocked in, end timing
+
+  const button = await Promise.race([
+    page.waitForSelector(
+      "xpath///button[span/span[@title='Fichar desde casa']]"
+    ),
+    page.waitForSelector("xpath///button[span[text()='Detener']]"),
+  ]);
+
+  if (await page.$("xpath///button[span/span[@title='Fichar desde casa']]")) {
+    const startButton = await page.$(
+      "xpath///button[span/span[@title='Fichar desde casa']]"
+    );
+    await startButton.click();
+    console.log("Turn Started");
+  } else {
+    const endButton = await page.$("xpath///button[span[text()='Detener']]");
+    await endButton.click();
+    console.log("Turn Ended");
+  }
+
+  await sleep(1000);
+
+  console.log("Script ends");
+  await browser.close();
+})();
